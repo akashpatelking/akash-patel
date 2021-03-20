55092c7a1ef883d9d7331c85b619d48a663b93c6
akashpatelking <77241335+akashpatelking@users.noreply.github.com>
2021-03-20 16:06:35 +0530

    Add files via upload

--- ALLHACKINGTOOL.sh
+++ ALLHACKINGTOOL.sh
@@ -10,7 +10,9 @@ echo -e "\e[4;31m Please Read Instruction Carefully !!! \e[0m"
  echo " Press Enter To Continue [ INTERNET CONNECTION ON ] "
 
  read a1
- apt install figlet toilet python curl -y
+ pkg install figlet 
+   
+pkg install toilet
  clear 
  echo -e "\e[1;31m"
 figlet ALLTOOL
