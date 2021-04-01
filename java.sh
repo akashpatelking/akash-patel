60b7ad7dbe276e3b7f890a5ae81d1f2dc4632796
akashpatelking <77241335+akashpatelking@users.noreply.github.com>
2021-04-01 20:16:08 +0530

    Add files via upload

--- java.sh
+++ java.sh
@@ -13,6 +13,381 @@ clear
 echo -e '\033[31;40;1m Plese wait link creat !'
    sleep 5    
    clear
+   echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo""
+echo"" 
+echo""
+echo""
+echo""
+echo""
+echo  ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo""
+echo""
+echo""
+echo""
+echo "               "/█1%...................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo  ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█4%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█8%......................./
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██10%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██12%....................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███15%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo ' 
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/█████19%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "               "/██████28%..................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████30%................../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/██████████40%................/
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/█████████████50%............./
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/████████████████60%........../
+sleep 0.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo '
+                     ┬  ┌─┐┌─┐┌┬┐┬┌┐┌┌─┐
+                     │  │ │├─┤ │││││││ ┬
+                     ┴─┘└─┘┴ ┴─┴┘┴┘└┘└─┘
+'
+echo " "
+echo " "
+echo " "
+echo " "
+echo "                "/███████████████████88%......./
+sleep 2.0
+clear
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+echo " "
+clear
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo "" 
+echo "               "[██████████████████████ 100%]
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+echo ""
+sleep 1.0
+clear 
 echo -e "\e[1;32m [ ↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↓↑ \e[0m"
  echo " "
  echo " " 
